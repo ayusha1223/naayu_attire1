902623b59ec350acd38701851a4ffc090ecee5ce
ayusha1223 <ayushathapa425@gmail.com>
2026-01-01 18:31:34 +0545

    auth_repository

---
+++ lib/features/auth/data/repositories/auth_repository.dart
@@ -0,0 +1,18 @@
+
+import 'package:dartz/dartz.dart';
+import 'package:naayu_attire1/core/error/failure.dart';
+import 'package:naayu_attire1/features/auth/domain/entities/auth_entity.dart';
+
+
+abstract interface class IAuthRepository {
+  Future<Either<Failure, bool>> register(AuthEntity entity);
+
+  Future<Either<Failure, AuthEntity>> login(
+    String email,
+    String password,
+  );
+
+  Future<Either<Failure, AuthEntity>> getCurrentUser();
+
+  Future<Either<Failure, bool>> logout();
+}
