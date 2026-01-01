bfcdb83c45dd60c962d279b7907488f51a86f208
ayusha1223 <ayushathapa425@gmail.com>
2026-01-01 18:33:54 +0545

    hive table constant

---
+++ lib/core/constants/hive_table_constants.dart
@@ -0,0 +1,11 @@
+class HiveTableConstant {
+  // Private constructor
+  HiveTableConstant._();
+
+  // Database name (project-specific)
+  static const String dbName = 'naayu_attire_db';
+
+  // 🔐 AUTH TABLE (Login / Signup)
+  static const int authTypeId = 0;
+  static const String authTable = 'auth_table';
+}
