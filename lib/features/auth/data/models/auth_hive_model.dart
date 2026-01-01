75b4a3274bd9cafbd8616fc456944bbf8d719146
ayusha1223 <ayushathapa425@gmail.com>
2026-01-01 18:26:00 +0545

    Auth_hive

---
+++ lib/features/auth/data/models/auth_hive_model.dart
@@ -0,0 +1,40 @@
+import 'package:hive/hive.dart';
+import '../../domain/entities/auth_entity.dart';
+
+part 'auth_hive_model.g.dart';
+
+@HiveType(typeId: 1)
+class AuthHiveModel extends HiveObject {
+  @HiveField(0)
+  final String fullName;
+
+  @HiveField(1)
+  final String email;
+
+  @HiveField(2)
+  final String password;
+
+  AuthHiveModel({
+    required this.fullName,
+    required this.email,
+    required this.password,
+  });
+
+  // Convert Hive → Entity
+  AuthEntity toEntity() {
+    return AuthEntity(
+      fullName: fullName,
+      email: email,
+      password: password,
+    );
+  }
+
+  // Convert Entity → Hive
+  factory AuthHiveModel.fromEntity(AuthEntity entity) {
+    return AuthHiveModel(
+      fullName: entity.fullName,
+      email: entity.email,
+      password: entity.password,
+    );
+  }
+}
