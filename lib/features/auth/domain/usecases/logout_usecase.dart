884ab2ae044b6ab982c5fba75b31deac5b45566e
ayusha1223 <ayushathapa425@gmail.com>
2026-01-02 10:50:08 +0545

    Logout usecase

---
+++ lib/features/auth/domain/usecases/logout_usecase.dart
@@ -0,0 +1,13 @@
+import 'package:dartz/dartz.dart';
+import 'package:naayu_attire1/core/error/failure.dart';
+import 'package:naayu_attire1/features/auth/domain/repositories/auth_repository.dart';
+
+class LogoutUsecase {
+  final IAuthRepository repository;
+
+  LogoutUsecase(this.repository);
+
+  Future<Either<Failure, bool>> call() {
+    return repository.logout();
+  }
+}
