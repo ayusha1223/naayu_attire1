927b9d09a9a13682735a7bb422506b7425b7d252
ayusha1223 <ayushathapa425@gmail.com>
2026-01-13 20:23:02 +0545

    api client

---
+++ lib/core/api/api_client.dart
@@ -0,0 +1,29 @@
+import 'package:dio/dio.dart';
+import 'api_endpoints.dart';
+
+class ApiClient {
+  late final Dio dio;
+
+  ApiClient() {
+    dio = Dio(
+      BaseOptions(
+        baseUrl: ApiEndpoints.baseUrl,
+        connectTimeout: const Duration(seconds: 30),
+        receiveTimeout: const Duration(seconds: 30),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      ),
+    );
+
+    // Logging (very good for viva)
+    dio.interceptors.add(
+      LogInterceptor(
+        request: true,
+        requestBody: true,
+        responseBody: true,
+        error: true,
+      ),
+    );
+  }
+}
