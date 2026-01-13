2399d844448cc7f7b0ea1138b6bec9ee54e612fc
ayusha1223 <ayushathapa425@gmail.com>
2026-01-13 20:23:39 +0545

    api endpoints

---
+++ lib/core/api/api_endpoints.dart
@@ -0,0 +1,17 @@
+class ApiEndpoints {
+  ApiEndpoints._();
+
+  // ================= Base URL =================
+  // Android Emulator
+  static const String baseUrl = 'http://10.0.2.2:3000/api/v1';
+
+  // Physical Device (example)
+  // static const String baseUrl = 'http://192.168.1.5:3000/api/v1';
+
+  // ================= Auth Endpoints =================
+  static const String studentRegister = '/students/register';
+  static const String studentLogin = '/students/login';
+
+  // ================= Other Endpoints (future) =================
+  static const String students = '/students';
+}
