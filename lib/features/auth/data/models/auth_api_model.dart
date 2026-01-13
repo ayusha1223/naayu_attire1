7bce4b1656d7ad8999e9c8e797c3ff30415e204f
ayusha1223 <ayushathapa425@gmail.com>
2026-01-13 20:08:28 +0545

    auth api model

---
+++ lib/features/auth/data/models/auth_api_model.dart
@@ -0,0 +1,35 @@
+
+
+import 'package:naayu_attire1/features/auth/domain/entities/auth_entity.dart';
+
+class AuthRemoteModel {
+  final String id;
+  final String name;
+  final String email;
+  final String token;
+
+  AuthRemoteModel({
+    required this.id,
+    required this.name,
+    required this.email,
+    required this.token,
+  });
+
+  factory AuthRemoteModel.fromJson(Map<String, dynamic> json) {
+    return AuthRemoteModel(
+      id: json['_id'],
+      name: json['name'],
+      email: json['email'],
+      token: json['token'],
+    );
+  }
+
+AuthEntity toEntity() {
+  return AuthEntity(
+    id: id,
+    fullName: name,
+    email: email,
+    password: '', // or '' if API does not return it
+  );
+}
+ }
