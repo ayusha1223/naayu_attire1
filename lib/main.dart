1d3257da873779bb3b6f3f5e4e480d51c83ac314
ayusha1223 <ayushathapa425@gmail.com>
2026-01-29 20:09:45 +0545

    Main dart

---
+++ lib/main.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import 'package:naayu_attire1/core/api/api_client.dart';
+import 'package:naayu_attire1/core/services/storage/token_service.dart';
+import 'package:naayu_attire1/features/auth/data/datasources/remote/auth_remote_datasource.dart';
+import 'package:naayu_attire1/features/auth/data/repositories/auth_repository_impl.dart';
+import 'package:naayu_attire1/features/auth/presentation/view_model/auth_view_model.dart';
+import 'package:naayu_attire1/features/onboarding/presentation/screens/onboarding_main.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  
+  final sharedPreferences = await SharedPreferences.getInstance();
+
+
+  final apiClient = ApiClient();
+  final tokenService = TokenService(sharedPreferences);
+
+  
+  final authDatasource = AuthRemoteDatasourceImpl(apiClient);
+  final authRepository = AuthRepositoryImpl(authDatasource);
+
+  runApp(
+    MultiProvider(
+      providers: [
+        
+        Provider<TokenService>(
+          create: (_) => tokenService,
+        ),
+
+       
+        ChangeNotifierProvider(
+          create: (_) => AuthViewModel(authRepository),
+        ),
+      ],
+      child: const MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Naayu Attire',
+      theme: ThemeData(
+        primarySwatch: Colors.pink,
+      ),
+      home: const OnboardingMain(),
+    );
+  }
+}
