00598df8b6d1713d5865e358edb3d53be8570980
ayusha1223 <ayushathapa425@gmail.com>
2026-01-31 10:32:49 +0545

    login use case tes

--- test/fake/fake_token_service.dart
+++
@@ -1,5 +0,0 @@
-class FakeTokenService {
-  String? getToken() {
-    return 'fake-token';
-  }
-}
---
+++ test/features/auth/domain/usecases/login_usecase_test.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:naayu_attire1/features/auth/domain/usecases/login_usecase.dart';
+
+void main() {
+  test('LoginUsecase class exists', () {
+    expect(LoginUsecase, isNotNull);
+  });
+}
