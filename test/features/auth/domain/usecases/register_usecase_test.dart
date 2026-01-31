bb0c3b3a68a10faa07cc57ead27bab0de807b429
ayusha1223 <ayushathapa425@gmail.com>
2026-01-31 10:34:03 +0545

    register use case

---
+++ test/features/auth/domain/usecases/register_usecase_test.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:naayu_attire1/features/auth/domain/usecases/register_usecase.dart';
+
+void main() {
+  test('RegisterUsecase class exists', () {
+    expect(RegisterUsecase, isNotNull);
+  });
+}
